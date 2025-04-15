7b4551743b3610283bdc7f3831fd765295c366df
Nikita Sirotenko <nick.sirotenko@yandex.ru>
2025-04-15 13:18:13 +0600

    Print start sysyem

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,41 +1,31 @@
 [bits 16]
 [org 0x7c00]
 
-
 start:
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov sp, 0x7c00
+    mov ah, 0xe
+    mov si, message
+    call print
+    jmp $
 
 print:
-    mov ah, 0x13
-    mov al, 1
-    mov bx, 0xa
-    xor dx, dx 
-    mov bp, MESSAGE
-    mov cx, MESSAGE_LEN
+    mov bx, 0
+.loop:
+    mov al, [si]
+    cmp al, 0
+    je .done
+    call print_char
+    inc si
+    jmp .loop
+.done:
+    ret
+
+print_char:
     int 0x10
+    ret
 
-end:
-    hlt
-    jmp end
+message: db 'Hello, nikxos', 0
+times 510 - ($ - $$) db 0               ; minimum 510 bytes set 0
 
-MESSAGE: db "Hello"
-MESSAGE_LEN: equ $-MESSAGE
-
-
-times (0x1be-($-$$)) db 0
-
-db 80h
-db 0,2,0
-db 0f0h
-db 0ffh,0ffh,0ffh
-dd 1
-dd (20*16*63-1) 
-
-times (16*3) db 0
-
+; Shows that this is boot sector
 db 0x55
-dd 0xaa
+db 0xaa
