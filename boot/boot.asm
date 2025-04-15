c152b6ae485014f7341ae6df6d5c2425c1d92066
Nikita Sirotenko <nick.sirotenko@yandex.ru>
2025-04-15 14:10:42 +0600

    Small improve bootloader

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,20 +1,31 @@
 [bits 16]
-[org 0x7c00]
+[org 0]
 
-start:
+jmp 0x7c0:start                         ; Change code segment to 0x7c0(because our code start from 0)
+
+start:      ; That move just for correct offest out offset while bootload
+    cli                                 ; Diable and clear interrups
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax                          ; Set up our stack from 0x7c00 -> 0x00 (stack grow down)
+    mov sp, 0x7c00
+    sti                                 ; Turn on interrups
     mov ah, 0xe
     mov si, message
     call print
     jmp $
 
+; move to diff file
 print:
     mov bx, 0
 .loop:
-    mov al, [si]
+    mov al, [si]                        ; put current char from message
     cmp al, 0
     je .done
     call print_char
-    inc si
+    inc si                              ; move to the next char
     jmp .loop
 .done:
     ret
